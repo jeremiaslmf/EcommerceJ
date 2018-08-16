16dbbde85d93dde46954d938185b50c3a0b0e380
Jeremias Lima <jeremias.filho@totvs.com.br>
2018-08-16 11:03:18 -0300

    Criação do Repositório

---
+++ EcommerceJ.Helpers.Tests/GuardTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EcommerceJ.Helpers.Tests
+{
+    [TestClass]
+    public class GuardTests
+    {
+        [TestMethod]
+        public void Guard_ForValidId_Valid()
+        {
+            Guard.ForValidId()
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Guard_ForNullOrEmpty_Erro_Quando_Em_Branco()
+        {
+            Guard.ForNullOrEmpty("", "Valor não pode ser vazio.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Guard_ForNullOrEmpty_null()
+        {
+            Guard.ForNullOrEmpty(null, "Valor não pode ser vazio.");
+        }
+
+        
+    }
+}
---
+++ EcommerceJ.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcommerceJ.Web.MvcApplication" Language="C#" %>
